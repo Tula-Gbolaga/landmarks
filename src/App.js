d260be4b4eca167026180a6fe9d3e62b960e58a9
Tula-Gbolaga <oguntulagbolag@gmail.com>
2024-07-17 01:02:08 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Landmarks In the world</header>
+      <p> New Landmarks project </p>
+    </div>
+  );
+}
+
+export default App;
